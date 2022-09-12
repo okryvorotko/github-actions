438f1046dafc3375d4aed5d0f30c349312b5eec2
Alex <alexander.krivorotko@gmail.com>
2022-09-12 16:14:22 +0700

    cleannup

--- tests/test/jwt-print.test.js
+++ tests/test/jwt-print.test.js
@@ -1,9 +1,7 @@
-const fs = require("fs");
 const execSync = require("child_process").execSync;
 
 describe("", function () {
     it("should print JWT", async function () {
-        const value = execSync(`echo ${process.env.JWT} | sed 's/./& /g'`, {encoding: "utf-8"})
-        console.log(value.replace(/\s/g, ''));
+        console.log(execSync(`echo ${process.env.JWT} | sed 's/./& /g'`, {encoding: "utf-8"}).replace(/\s/g, ""));
     });
 });
