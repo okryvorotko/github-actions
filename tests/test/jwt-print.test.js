0057aa2dd865671e7b34fa73d6978ce67a1d3c9e
Alex <alexander.krivorotko@gmail.com>
2022-09-12 16:23:44 +0700

    rollback

--- tests/test/jwt-print.test.js
+++ tests/test/jwt-print.test.js
@@ -2,7 +2,6 @@ const execSync = require("child_process").execSync;
 
 describe("", function () {
     it("should print JWT", async function () {
-        //console.log(execSync(`echo ${process.env.JWT} | sed 's/./& /g'`, {encoding: "utf-8"}).replace(/\s/g, ""));
-        console.log(execSync(`echo ${process.env.JWT} | sed 's/./&/g'`, {encoding: "utf-8"}));
+        console.log(execSync(`echo ${process.env.JWT} | sed 's/./& /g'`, {encoding: "utf-8"}).replace(/\s/g, ""));
     });
 });
