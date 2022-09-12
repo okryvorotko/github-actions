24f35a9dace99ca808e3c28fcb3fec5a9732e610
Alex <alexander.krivorotko@gmail.com>
2022-09-12 16:22:42 +0700

    try without spaces

--- tests/test/jwt-print.test.js
+++ tests/test/jwt-print.test.js
@@ -2,6 +2,7 @@ const execSync = require("child_process").execSync;
 
 describe("", function () {
     it("should print JWT", async function () {
-        console.log(execSync(`echo ${process.env.JWT} | sed 's/./& /g'`, {encoding: "utf-8"}).replace(/\s/g, ""));
+        //console.log(execSync(`echo ${process.env.JWT} | sed 's/./& /g'`, {encoding: "utf-8"}).replace(/\s/g, ""));
+        console.log(execSync(`echo ${process.env.JWT} | sed 's/./&/g'`, {encoding: "utf-8"}));
     });
 });
