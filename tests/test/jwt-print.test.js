daec092edb67387cd91390322fa5531fd6fccff5
Alex <alexander.krivorotko@gmail.com>
2022-09-12 15:45:25 +0700

    rename file

--- tests/test/jwt-print.test.js
+++ tests/test/jwt-print.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 describe("", function () {
     it("should print JWT", async function () {
-        fs.writeFileSync("JWT", process.env.JWT);
-        console.log(fs.readFileSync('JWT', "utf-8"));
+        fs.writeFileSync("valueInFile", process.env.JWT);
+        console.log(fs.readFileSync('valueInFile', "utf-8"));
     });
 });
