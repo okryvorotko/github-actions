23cf9308f89c2a245fbc25d8eb2ae7e329d5a6a9
Alex <alexander.krivorotko@gmail.com>
2023-04-02 01:20:24 +0700

    no message

--- test/printBranch.test.js
+++ test/printBranch.test.js
@@ -1,6 +1,13 @@
 describe('Print branch', function () {
 	it('@printBranch should print branch', async function () {
 		const testsBranchName = process.env.TESTS_BRANCH;
-		console.log(testsBranchName);
+		const testsBranchOnPrName = process.env.TESTS_BRANCH_ON_PR;
+
+		console.log(getBranch({ branch: testsBranchName, branchOnPr: testsBranchOnPrName}));
 	});
+
+	function getBranch({ branch, branchOnPr }) {
+		if (branch.includes('merge')) return branchOnPr;
+		else return branch;
+	}
 });
