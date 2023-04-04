ed1a9a3e41472e84e1bfcd9282f71319c733b0c2
Alex <alexander.krivorotko@gmail.com>
2023-04-04 10:43:32 +0700

    cleanup

--- test/exclude.test.js
+++
@@ -1,7 +0,0 @@
-describe("Exclude with params tests @exclude", function () {
-    it("should exclude", async function () {
-    });
-
-    it("should exclude", async function () {
-    });
-});
--- test/exclude2.test.js
+++
@@ -1,7 +0,0 @@
-describe("Exclude with params tests @exclude2", function () {
-    it("should exclude", async function () {
-    });
-
-    it("should exclude", async function () {
-    });
-});
