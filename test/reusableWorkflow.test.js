0d59b3a888649c856227619c9dd1a79aaeb1b19c
Alex <alexander.krivorotko@gmail.com>
2023-04-04 09:30:48 +0700

    finalize

--- test/reusableWorkflow.test.js
+++ test/reusableWorkflow.test.js
@@ -1,6 +1,7 @@
 it('@reusable1 reusable workflow', async function () {
-	console.log(`File content: ${process.env.FILE}`);
+	console.log(`Shared variable: ${process.env.sharedVar}`);
 });
 
 it('@reusable2 reusable2 workflow', async function () {
+	console.log(`Shared variable: ${process.env.sharedVar}`);
 });
