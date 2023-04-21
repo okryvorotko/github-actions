09385719a9a7d20c2df6d7c38a614d2601e53bcd
Alex <alexander.krivorotko@gmail.com>
2023-04-21 22:13:32 +0700

    ref

--- test/printBranch.test.js
+++ test/printBranch.test.js
@@ -1,6 +1,10 @@
 describe('Print branch', function () {
 	it('@printBranch should print branch', async function () {
-		const testsBranchName = process.env.TESTS_BRANCH;
-		console.log(testsBranchName);
+		const headref = process.env.HEAD_REF;
+		const baseref = process.env.BASE_REF;
+		const ref = process.env.REF;
+		console.log(`headref: ${headref}`);
+		console.log(`baseref: ${baseref}`);
+		console.log(`ref: ${ref}`);
 	});
 });
