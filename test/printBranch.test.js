d79dd0fd45f928307fd137c2f3fe8ddb6a248a6f
Alex <alexander.krivorotko@gmail.com>
2023-04-21 22:19:41 +0700

    ref ame

--- test/printBranch.test.js
+++ test/printBranch.test.js
@@ -3,6 +3,7 @@ describe('Print branch', function () {
 		const headref = process.env.HEAD_REF;
 		const baseref = process.env.BASE_REF;
 		const ref = process.env.REF;
+		const refname = process.env.REF_NAME;
 		console.log(`headref: ${headref}`);
 		console.log(`baseref: ${baseref}`);
 		console.log(`ref: ${ref}`);
